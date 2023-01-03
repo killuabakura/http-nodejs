d733f89a5b7461a12d893ffbbdedacef8c1b599e
killuabakura <49504478+killuabakura@users.noreply.github.com>
2023-01-03 17:46:53 -0300

    Add files via upload

---
+++ shell.js
@@ -0,0 +1,36 @@
+/**
+ * ==== wwebjs-shell ====
+ * Used for quickly testing library features
+ * 
+ * Running `npm run shell` will start WhatsApp Web with headless=false
+ * and then drop you into Node REPL with `client` in its context. 
+ */
+
+const repl = require('repl');
+
+const { Client, LocalAuth } = require('./index');
+
+const client = new Client({
+    puppeteer: { headless: false }, 
+    authStrategy: new LocalAuth()
+});
+
+console.log('Inicializando...');
+
+client.initialize();
+
+client.on('qr', () => {
+    console.log('Escaneie o QRCode para iniciar sua sessão.');
+});
+
+client.on('authenticated', (session) => {
+    console.log(JSON.stringify(session));
+});
+
+client.on('ready', () => {
+    const shell = repl.start('wwebjs> ');
+    shell.context.client = client;
+    shell.on('exit', async () => {
+        await client.destroy();
+    });
+});
